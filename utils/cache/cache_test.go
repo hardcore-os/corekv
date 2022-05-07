d1868a265dc4b0484d06367b6db3471614531afd
yangkai.04 <yangkai.04@bytedance.com>
2022-05-07 11:13:21 +0800

    [feat] TestCacheSameKey

--- utils/cache/cache_test.go
+++ utils/cache/cache_test.go
@@ -26,3 +26,20 @@ func TestCacheBasicCRUD(t *testing.T) {
 
 	}
 }
+
+
+func TestCacheSameKey(t *testing.T) {
+	cache := NewCache(500)
+	key := "one"
+	for i := 0; i < 10; i++ {
+		val := fmt.Sprintf("val%d", i)
+		cache.Set(key, val)
+		res, ok := cache.Get(key)
+		if ok {
+			assert.Equal(t, val, res)
+			continue
+		}
+		assert.Equal(t, res, nil)
+	}
+}
+
